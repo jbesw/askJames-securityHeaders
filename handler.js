fd50812b43a294cee10d1716d5c8a587225a2235
James Beswick <jamesbeswick@gmail.com>
2019-02-08 11:30:45 -0500

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,20 @@
+
+// Add security headers to CloudFront distribution - apply to Origin Response event.
+
+exports.handler = async (event) => {
+
+    // Get contents of response
+    const response = event.Records[0].cf.response
+    const headers = response.headers
+
+    // Set new headers 
+     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
+     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]
+     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]
+     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}]
+     headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]
+
+    // Return modified response
+    return response
+
+}
