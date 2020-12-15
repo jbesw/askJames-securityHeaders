b8197c934a0bbb391a156f36316db04e7f41703e
Chatree Kunjai <chatree.kunjai@tesco.com>
2020-12-15 17:14:28 +0700

    Adjust syntax and indent.

--- handler.js
+++ handler.js
@@ -6,11 +6,31 @@ exports.handler = async (event) => {
     let headers = response.headers
 
     // Set new headers 
-    headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
-    headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]
-    headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]
-    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}]
-    headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]
+    // Fix syntax by this ref : https://stackoverflow.com/questions/45715973/the-lambda-function-returned-an-invalid-request-or-response-to-cloudfront
+    headers['strict-transport-security'] = [{
+        key: 'Strict-Transport-Security', 
+        value: "max-age=31536000; includeSubdomains; preload"
+    }];
+
+    headers['content-security-policy'] = [{
+        key: 'Content-Security-Policy', 
+        value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+    }];
+    
+    headers['x-content-type-options'] = [{
+        key: 'X-Content-Type-Options',
+        value: "nosniff"
+    }];
+    
+    headers['x-frame-options'] = [{
+        key: 'X-Frame-Options',
+        value: "DENY"
+    }];
+    
+    headers['referrer-policy'] = [{
+        key: 'Referrer-Policy',
+        value: "same-origin"
+    }];
 
     // Return modified response
     return response
